65e2546ce5f44b22839d1b5907d6fa6aad657ce6
unknown <gaetan@AESONDEV.COM>
2017-12-16 22:05:28 +0000

    Build all Project from Folder

--- src/EdmoBuildTools.psd1
+++ src/EdmoBuildTools.psd1
@@ -12,7 +12,7 @@
 RootModule = '.\EdmoBuildTools.psm1'
 
 # Version number of this module.
-ModuleVersion = '1.1.1'
+ModuleVersion = '1.1.35'
 
 # Supported PSEditions
 # CompatiblePSEditions = @()
@@ -70,7 +70,8 @@ PowerShellVersion = '4.0'
 
 # Functions to export from this module, for best performance, do not use wildcards and do not delete the entry, use an empty array if there are no functions to export.
 FunctionsToExport = 'Get-Test', 'Get-LocalDotNetCoreVersion', 
-               'Test-DotNetCoreFunctionsAreLoaded'
+               'Test-DotNetCoreFunctionsAreLoaded', 'New-DotNetCoreBuild', 
+               'Start-UnitTests'
 
 # Cmdlets to export from this module, for best performance, do not use wildcards and do not delete the entry, use an empty array if there are no cmdlets to export.
 CmdletsToExport = @()
--- src/PublicFunctions/DotNetCore/DotNetCore.ps1
+++ src/PublicFunctions/DotNetCore/DotNetCore.ps1
@@ -1,11 +1,79 @@
 
-function Get-LocalDotNetCoreVersion{
-  $exe = (Get-Command dotnet)
-  $version = $exe.Version
-  return  $version
+function Get-LocalDotNetCoreVersion {
+    $exe = (Get-Command dotnet)
+    $version = $exe.Version
+    return  $version
 }
 
+function New-DotNetCoreBuild {
+    param (
+        [string]$Location,
+        [string]$Configuration,
+        [bool]$IncludeDependentProjects = $true,
+        [bool]$RestorePackages = $true
+    )
+
+   
+    $dotnetExe = (get-command dotnet).Source
+   
+    $TestProjects = Get-ChildItem -Path $Location -Recurse -Filter "*.csproj"
+    foreach ($Project in $TestProjects) {
+        $path = $Project.Directory
+        Push-Location $path
+        $args = ""
+        if ($Configuration) {
+            $args += " -c $Configuration"
+        }
+        if ($IncludeDependentProjects -eq $false) {
+            $args += " --no-dependencies "
+        }
+        if ($RestorePackages -eq $false) {
+            $args += " --no-restore "
+        }
+
+        Write-Output "dotnet build $args"
+    
+        exec {
+            & $dotnetExe build "$args"
+        }
+
+        Pop-Location
+    }
+    
+   
+  
+}
+
+function Start-UnitTests {
+    param (
+        [string]$Location,
+        [string]$Configuration       
+    )
+
+    $dotnetExe = (get-command dotnet).Source
+
+    $TestProjects = Get-ChildItem -Path $Location -Filter "*.csproj"
+    foreach ($Project in $TestProjects) {
+        
+        Write-Output "Testing $Project"
+    }
+
+    Push-Location $Location   
+    $args = ""
+    if ($Configuration) {
+        $args += " -c $Configuration"
+    }   
+
+    Write-Output "dotnet test - $args"    
+    exec {
+        & $dotnetExe test "$args"
+    }
+    Pop-Location
+}
+
+
+
 function Test-DotNetCoreFunctionsAreLoaded {
-  return "ok"
+    return "ok"
 }
 
